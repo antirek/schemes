5fbe35452e8a3123e871ad65cef779d791960fc3
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-03-29 17:53:24 +0700

    fix

--- schemes/baseSchema.js
+++ schemes/baseSchema.js
@@ -4,24 +4,31 @@ var peer = require('./peerSchema');
 var user = require('./userSchema');
 
 
+var arrayOfTypes = ["peer", "ivr", "user", "timeweek", 
+                    "timeday", "incoming", "queue", 
+                    "group"];
+
+
 var baseSchema = Joi.object().keys({
     "_id": Joi.string().required(),
     "extension": Joi.string(),
-    "type": Joi.string().required().valid("peer", "ivr", "user", "timeweek", "timeday", "incoming", "queue", "group"),
+    "type": Joi.string().required().valid(arrayOfTypes),
     "context": Joi.string(),
     "title" : Joi.string().required(),
     "vpbxId": Joi.string().required(),
+
     "params": Joi.any()
-    	.when(
-    		'type', {is: 'peer', then: peer.params}
-    	)
-    	.when(
-    		'type', {is: 'user', then: user.params}
-    	),
+        .when(
+            'type', {is: 'peer', then: peer.params}
+        )
+        .when(
+            'type', {is: 'user', then: user.params}
+        ),
+        
     "routes": Joi.any()
-    	.when(
-    		'type', {is: 'peer', then: peer.routes}
-    	)
+        .when(
+            'type', {is: 'peer', then: peer.routes}
+        )
 });
 
 module.exports = baseSchema;
