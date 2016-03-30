3f73d17cdc26f52f5dbc4a92f461bc873f1f95b1
k666r <k666r6@gmail.com>
2016-03-30 13:37:32 +0700

    Добавил точку входа

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
-
 var Joi = require('joi');
-
 var baseSchema = require('./schemes/baseSchema');
 
-var peer1 = require('./objects/valid/peer/peer1.json');
-var user1 = require('./objects/valid/user/user1.json');
 
-
-var validate = function (object, schema) {
-    return new Promise(function (resolve, reject) {
-        Joi.validate(object, schema, function (err, res) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        })
-    });
+var validate = function (object, schema, cb) {
+    if (cb) {
+        Joi.validate(object, baseSchema, cb);
+    } else {
+        return new Promise(function (resolve, reject) {
+            Joi.validate(object, baseSchema, function (err, res) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(res);
+                }
+            })
+        });
+    }
 };
 
 
-var arr = [
-    validate(peer1, baseSchema),
-    validate(user1, baseSchema),
-];
-
-Promise.all(arr)
-.then(function (res) {
-    console.log('all good');
-})
-.catch(console.log);
+module.exports = validate
