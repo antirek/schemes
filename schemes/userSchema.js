b2ff3b2f5658310a1491d0b14769f39c068cbec8
k666r <k666r6@gmail.com>
2016-04-01 13:41:01 +0700

    change user scheme

--- schemes/userSchema.js
+++ schemes/userSchema.js
@@ -1,12 +1,22 @@
-
 var Joi = require('joi');
 var routes = require('./routes');
 
 var user = {
-	params: Joi.any(),
-	routes: Joi.object().keys({
-		default: routes.standard
-	})
+    params: Joi.object().keys({
+        "mobile": Joi.string(),
+        "email": Joi.string().email(),
+        "operator": Joi.boolean(),
+        "objects": Joi.array().items(
+            Joi.object().keys({
+                "type": Joi.string().required().valid(["peer", "voicemail"]),
+                "id": Joi.string().required()
+            })
+        )
+
+    }),
+    routes: Joi.object().keys({
+        default: routes.standard
+    })
 };
 
 
