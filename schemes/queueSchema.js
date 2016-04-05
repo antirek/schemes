8251da0e571378574dcabd9986c1c6a2caba1989
k666r <k666r6@gmail.com>
2016-04-05 13:36:16 +0700

    Поменял схему очереди

--- schemes/queueSchema.js
+++ schemes/queueSchema.js
@@ -1,12 +1,25 @@
-
 var Joi = require('joi');
 var routes = require('./routes');
 
 var queue = {
-	params: Joi.object().keys({
-		name: Joi.string().required()
-	}),
-	routes: Joi.any()
+    params: Joi.object().keys({
+        name: Joi.string().required(),
+        operators: Joi.object().keys({
+            static: Joi.array().items(
+                Joi.object().keys({
+                    type: Joi.string().required().allow(['peer', 'outbound']),
+                    id: Joi.string().required()
+                })
+            ),
+            dynamic: Joi.array().items(
+                Joi.object().keys({
+                    type: Joi.string().required().allow(['user']),
+                    id: Joi.string().required()
+                })
+            )
+        })
+    }),
+    routes: Joi.any()
 };
 
 
