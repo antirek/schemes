94dba6aa044820066c3dcab1ccc8ad13a2249717
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-04-12 12:16:39 +0700

    change incoming schema

--- schemes/incomingSchema.js
+++ schemes/incomingSchema.js
@@ -4,8 +4,9 @@ var routes = require('./routes');
 
 var incoming = {
 	params: Joi.object().keys({
-		record: Joi.boolean(),
-		lines: Joi.string().required()
+		record: Joi.boolean().required().default(false),
+		lines: Joi.string().required(),
+		checkBlacklist: Joi.boolean().required().default(false)
 	}),
 	routes: Joi.object().keys({
 		"default": routes.standard
