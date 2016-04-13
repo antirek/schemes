dd8da2de35ab39c8c819674bca8acffeb11eb724
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-04-13 14:36:11 +0700

    add outbound rules

--- schemes/peerSchema.js
+++ schemes/peerSchema.js
@@ -6,7 +6,7 @@ var peer = {
 	params: Joi.object().keys({
 		"peername": Joi.string().required(),
 		"secret": Joi.string().required(),
-		"record": Joi.string().required().valid(['true', 'false']),
+		"record": Joi.boolean().required().default(false),
 		"type": Joi.string().required().valid('friend'),
 		"host": Joi.string().required().valid('dynamic'),
 		"qualify": Joi.string().required(),
@@ -14,7 +14,14 @@ var peer = {
 		"outgoingNumber": Joi.string().required(),
 		"disallow": Joi.string().required(),
 		"allow": Joi.string().required(),
-		"lines": Joi.string().required()
+		"lines": Joi.string().required(),
+		"allowOutbound": Joi.boolean().required().default(true),
+		"restrictedPrefixes": Joi.array().items(
+			Joi.string().required()
+		).sparse(),
+		"excludedPrefixes": Joi.array().items(
+			Joi.string().required()
+		).sparse()
 	}),
 	routes: Joi.object().keys({
 		"BUSY": routes.standard,
