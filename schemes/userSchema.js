20cbc5f68120c4d46bb021ea9946f3affb77e87f
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-04-28 14:53:32 +0700

    pincode required always autoQueueLogin is boolean

--- schemes/userSchema.js
+++ schemes/userSchema.js
@@ -7,26 +7,16 @@ var user = {
     params: Joi.object().keys({
         "mobile": Joi.string(),
         "email": Joi.string().email(),
-        "operator": Joi.boolean(),
-        "pincode": Joi.string().length(5).alphanum()
-            .when('operator', {is: true, then: Joi.required()}),
-        "objects": Joi.array().items(
-            Joi.object().keys({
-                "type": Joi.string().required().valid(["peer", "voicemail"]),
-                "id": Joi.string().required()
-            })
-        ),
+        "pincode": Joi.string().length(5).alphanum().required(),
+        "autoQueueLogin": Joi.boolean().required().default(false),
         "userkey": Joi.string().length(16).alphanum(),
         "currentInterface": Joi.string(),
-        "pause": Joi.boolean(),
-        "presenceState": Joi.string().required(),
-        "basePeerId": Joi.string().required().allow(null) 
+        "basePeerId": Joi.string().required().allow(null)
     })
     .keys(userOptions),
     routes: Joi.object().keys({
         default: routes.standard
     })
 };
-
 
 module.exports = user;
