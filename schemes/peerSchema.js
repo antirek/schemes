f5e7f9609795bf3625b8bd5fe5f8bab15a283cbc
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-05-31 13:59:28 +0700

    location is not required

--- schemes/peerSchema.js
+++ schemes/peerSchema.js
@@ -17,7 +17,7 @@ var peer = {
         "lines": Joi.string().required(),
         "sendrpid": Joi.string().required().valid('yes'),
         "trustrpid": Joi.string().required().valid('yes'),
-        "location": Joi.string().required().allow('')
+        "location": Joi.string().allow('')
     }).keys(userOptions),
     routes: Joi.object().keys({
         "BUSY": routes.standard,
