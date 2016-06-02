e7aea2605888414ead80691fc9dd10c3d12b87e0
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-06-02 15:54:36 +0700

    change ivr

--- schemes/ivrSchema.js
+++ schemes/ivrSchema.js
@@ -3,12 +3,18 @@ var Joi = require('joi');
 var routes = require('./routes');
 
 var ivr = {
-	params: Joi.object().keys({
-		"soundId": Joi.string().required(),
-		"maxdigits": Joi.string().required(),
-		"options": Joi.string().required()
-	}),
-	routes: Joi.array().items(routes.key)
+    params: Joi.object().keys({
+        "soundId": Joi.string().required(),
+        "timeout": Joi.string().required(),
+        "passToInternal": Joi.boolean().default(false),
+        "attempts": Joi.string().required(),
+        "answer": Joi.boolean().default(true),
+        "repeat": Joi.string().required()
+    }),
+    routes: Joi.object().keys({
+        keys: Joi.array().items(routes.key),
+        default: routes.standard
+    })
 };
 
 module.exports = ivr;
