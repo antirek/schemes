daba414ce81a0568e11eae69875f4e16e815cf5f
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-06-07 16:43:07 +0700

    fix vpbx settings

--- schemes/vpbxSettingsSchema.js
+++ schemes/vpbxSettingsSchema.js
@@ -4,17 +4,19 @@ var Joi = require('joi');
 var vpbxSettings = Joi.object().keys({
     "_id": Joi.string().required(),
     vpbxId: Joi.string().required(),
-    category: Joi.string().required().valid(['premium', 'standard']),
-    availableExternalCallerids: Joi.array().items(
-    	Joi.string().min(7).max(11)
-    ),
-    domain: Joi.string(),
-    outgoingCodes: Joi.array().items(
-    	Joi.object().keys({
-    		number: Joi.string().required(),
-    		code: Joi.string().required()
-    	})
-    )
+    params: Joi.object().keys({
+        category: Joi.string().required().valid(['premium', 'standard']),
+        availableExternalCallerids: Joi.array().items(
+            Joi.string().min(7).max(11)
+        ),
+        domain: Joi.string(),
+        outgoingCodes: Joi.array().items(
+            Joi.object().keys({
+                number: Joi.string().required(),
+                code: Joi.string().required()
+            })
+        )
+    })
 });
 
 module.exports = vpbxSettings;
