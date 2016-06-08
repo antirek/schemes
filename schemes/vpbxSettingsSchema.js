a2c03123617be36956a692e881993b1bf34c50eb
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-06-08 11:53:05 +0700

    fix

--- schemes/vpbxSettingsSchema.js
+++ schemes/vpbxSettingsSchema.js
@@ -6,13 +6,10 @@ var vpbxSettings = Joi.object().keys({
     vpbxId: Joi.string().required(),
     params: Joi.object().keys({
         category: Joi.string().required().valid(['premium', 'standard']),
-        availableExternalCallerids: Joi.array().items(
-            Joi.string().min(7).max(11)
-        ),
         domain: Joi.string(),
-        outgoingCodes: Joi.array().items(
+        availableExternalCallerids: Joi.array().items(
             Joi.object().keys({
-                number: Joi.string().required(),
+                callerid: Joi.string().required(),
                 code: Joi.string().required()
             })
         )
