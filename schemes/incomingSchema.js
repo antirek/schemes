8d9da2ce0e3e65fe8684bc31cfa69a11729da946
k666r <k666r6@gmail.com>
2016-11-22 12:16:04 +0700

    chanhe incoming sheme

--- schemes/incomingSchema.js
+++ schemes/incomingSchema.js
@@ -1,16 +1,19 @@
-
 var Joi = require('joi');
 var routes = require('./routes');
 
 var incoming = {
-	params: Joi.object().keys({
-		record: Joi.boolean().required().default(false),
-		lines: Joi.string().required(),
-		checkBlacklist: Joi.boolean().required().default(false)
-	}),
-	routes: Joi.object().keys({
-		"default": routes.standard
-	})
+    params: Joi.object().keys({
+        record: Joi.boolean().required().default(false),
+        lines: Joi.string().required(),
+        routingCode: Joi.string(),
+        queuelines: Joi.string(),
+        category: Joi.string(),
+        checkBlacklist: Joi.boolean().required().default(false)
+
+    }),
+    routes: Joi.object().keys({
+        "default": routes.standard
+    })
 };
 
 
