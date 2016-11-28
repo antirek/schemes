c9b6b9a63216dc739d0595119a664279b7f6511c
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-11-28 17:30:59 +0700

    fix

--- schemes/common/userOptions.js
+++ schemes/common/userOptions.js
@@ -3,10 +3,10 @@ var Joi = require('joi');
 
 var common = {    
     "restrictedPrefixes": Joi.array().items(
-        Joi.string().required()
+        Joi.string()
     ).sparse(),
     "excludedPrefixes": Joi.array().items(
-        Joi.string().required()
+        Joi.string()
     ).sparse(),    
     "record": Joi.boolean().required().default(false),
 };
