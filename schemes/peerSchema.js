90d4c44bfc6d79b9bce7d1206ab28b0e614ae573
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-02-02 11:41:06 +0700

    fix secret for peer

--- schemes/peerSchema.js
+++ schemes/peerSchema.js
@@ -7,7 +7,7 @@ var userOptions = require('./common/userOptions');
 var peer = {
     params: Joi.object().keys({
         "peername": Joi.string().required(),
-        "secret": Joi.string().required(),
+        "secret": Joi.string().valid(""),
         "type": Joi.string().required().valid('friend'),
         "host": Joi.string().required().valid('dynamic'),
         "qualify": Joi.string().required(),
