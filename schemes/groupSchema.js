5f5d8cc5a263f52b7332312f061d47ec0fff1a44
Tolyan <k666r6@gmail.com>
2017-08-04 09:46:26 +0700

    fix

--- schemes/groupSchema.js
+++ schemes/groupSchema.js
@@ -1,10 +1,11 @@
-
 var Joi = require('joi');
 var routes = require('./routes');
 
 var group = {
-	params: Joi.any(),
-	routes: Joi.array().items(routes.group)
+    params: Joi.object().keys({
+        musiconhold: Joi.string()
+    }),
+    routes: Joi.array().items(routes.group)
 };
 
 module.exports = group;
