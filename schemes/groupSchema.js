4e6757ca09fd06d97639e5955b8b79cff016af78
Tolyan <k666r6@gmail.com>
2017-08-09 11:12:16 +0700

    add outgoingNumber to group

--- schemes/groupSchema.js
+++ schemes/groupSchema.js
@@ -3,7 +3,8 @@ var routes = require('./routes');
 
 var group = {
     params: Joi.object().keys({
-        musiconhold: Joi.string()
+        musiconhold: Joi.string(),
+        outgoingNumber: Joi.string()
     }),
     routes: Joi.array().items(routes.group)
 };
